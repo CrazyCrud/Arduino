f6869012c6d28167b8e7f27482a3d2d9a439ed73
Constantin Lehenmeier <constantin.lehenmeier@stud.uni-regensburg.de>
2013-02-22 15:50:57 +0100

    Farbsensor & Magnet

--- _2102/_2102.ino
+++ _2102/_2102.ino
@@ -1,6 +1,10 @@
+#include "math.h"
+
 const int led_red = 2;
 const int led_blue = 4;
 const int led_green= 6;
+const int magnet_analog = 5;
+const int treshold = 100;
 int intens_red, intens_blue, intens_green;
 int sensorV;
 
@@ -12,8 +16,21 @@ void setup(){
 }
 
 void loop(){
+  if(isMagnetDetected()){
+    blinkLamps();
+    computeIntensity();
+  }
   blinkLamps();
   computeIntensity();
+}
+
+boolean isMagnetDetected(){
+  int magnetValue = analogRead(magnet_analog);
+  // Serial.println(magnetValue);
+  if(analogRead(magnet_analog) > 0){
+    return false;
+  }
+  return true;
 }
 
 void blinkLamps(){
@@ -46,26 +63,47 @@ void turnOffLight(int which){
 }
 
 void checkSensor(int which){
-  sensorV = analogRead(A0);
+  sensorV = analogRead(0);
   switch(which){
     case led_red:
-      Serial.print("Rot: ");    
+      intens_red = sensorV;
+      // Serial.print("R: ");    
       break;
     case led_blue:
-      Serial.print("Blau: ");
+      intens_blue = sensorV;
+      // Serial.print("B: ");
       break;
     case led_green:
-      Serial.print("Gruen: ");
+      intens_green = sensorV;
+      // Serial.print("G: ");
       break;  
   }
   Serial.println(sensorV); 
 }
 
-void checkSensor(){
-  sensorV = analogRead(A0);
-  Serial.println(sensorV);
+void computeIntensity(){
+    scale();
+    defineColor();
 }
 
-void computeIntensity(){
-    
+void scale(){
+    // Serial.println("Scaled Values");
+    intens_red = map(intens_red, 0, 1023, 255 , 0);
+    // Serial.println(intens_red);
+    intens_green = map(intens_green, 0, 1023, 255, 0);
+    // Serial.println(intens_green);
+    intens_blue = map(intens_blue, 0, 1023, 255, 0);
+    // Serial.println(intens_blue);
+}
+
+void defineColor(){
+    if(intens_red > intens_green && intens_red > intens_blue){
+      int r_g_diff = intens_red - intens_green;
+      int r_b_diff = intens_red - intens_blue;
+      if(r_g_diff > treshold && r_b_diff > treshold){
+        Serial.println("red");
+        return;
+      }
+    }
+    Serial.println("not color");
 }
