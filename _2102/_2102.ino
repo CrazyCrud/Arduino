e3b025f0e5361a21317cac4528d597d26a61b879
Constantin Lehenmeier <constantin.lehenmeier@stud.uni-regensburg.de>
2013-02-25 16:55:42 +0100

    Farbsensor & Magnet

--- _2102/_2102.ino
+++ _2102/_2102.ino
@@ -1,11 +1,20 @@
 #include "math.h"
+#include <StandardCplusplus.h>
+#include <vector>
+#include <string>
+#include <iterator>
+using namespace std;
 
 const int led_red = 2;
 const int led_blue = 4;
 const int led_green= 6;
 const int magnet_analog = 5;
-const int treshold = 100;
+const int magnet_digital = 8;
+const int treshold_color = 60;
+const int treshold_magnet = 30;
+const int magnet_values = 5;
 int intens_red, intens_blue, intens_green;
+vector<int> intens_magnet;
 int sensorV;
 
 void setup(){
@@ -13,45 +22,70 @@ void setup(){
   pinMode(led_red, OUTPUT);
   pinMode(led_blue, OUTPUT);
   pinMode(led_green, OUTPUT);
+  pinMode(magnet_digital, INPUT);
 }
 
 void loop(){
+  delay(250);
   if(isMagnetDetected()){
     blinkLamps();
     computeIntensity();
   }
-  blinkLamps();
-  computeIntensity();
 }
 
 boolean isMagnetDetected(){
   int magnetValue = analogRead(magnet_analog);
-  // Serial.println(magnetValue);
-  if(analogRead(magnet_analog) > 0){
-    return false;
+  manageMagnetValues(magnetValue);
+  magnetValue = getAvgMagnetValue();
+  Serial.print("Avg. value: ");
+  Serial.println(magnetValue);
+  if(magnetValue < treshold_magnet){
+    return true;
   }
-  return true;
+  return false;
+}
+
+void manageMagnetValues(int value){
+  intens_magnet.insert(intens_magnet.begin(), value);
+  if(intens_magnet.size() > magnet_values){
+    intens_magnet.pop_back();
+  }
+}
+
+int getAvgMagnetValue(){
+  float value = 0.0;
+  int vecSize = intens_magnet.size();
+  for(int i = 0; i < vecSize; i++){
+    value += intens_magnet.at(i);
+  }
+  value = value / vecSize;
+  value = roundValue(value);
+  return int(value);
+}
+
+float roundValue(float value){
+  return value<0 ? value - 0.5: value + 0.5;
 }
 
 void blinkLamps(){
   turnOnLight(led_red);
-  delay(1000);
+  delay(500);
   checkSensor(led_red);
-  delay(2000);
+  delay(1000);
   turnOffLight(led_red);
-  delay(2000);
+  delay(1000);
   turnOnLight(led_blue);
-  delay(1000);
+  delay(500);
   checkSensor(led_blue);
-  delay(2000);
-  turnOffLight(led_blue);
-  delay(2000);
-  turnOnLight(led_green);
   delay(1000);
+  turnOffLight(led_blue);
+  delay(1000);
+  turnOnLight(led_green);
+  delay(500);
   checkSensor(led_green);
-  delay(2000);
+  delay(1000);
   turnOffLight(led_green);
-  delay(2000);
+  delay(1000);
 }
 
 void turnOnLight(int which){
@@ -67,15 +101,15 @@ void checkSensor(int which){
   switch(which){
     case led_red:
       intens_red = sensorV;
-      // Serial.print("R: ");    
+      Serial.print("R: ");    
       break;
     case led_blue:
       intens_blue = sensorV;
-      // Serial.print("B: ");
+      Serial.print("B: ");
       break;
     case led_green:
       intens_green = sensorV;
-      // Serial.print("G: ");
+      Serial.print("G: ");
       break;  
   }
   Serial.println(sensorV); 
@@ -100,10 +134,10 @@ void defineColor(){
     if(intens_red > intens_green && intens_red > intens_blue){
       int r_g_diff = intens_red - intens_green;
       int r_b_diff = intens_red - intens_blue;
-      if(r_g_diff > treshold && r_b_diff > treshold){
-        Serial.println("red");
+      if(r_g_diff > treshold_color && r_b_diff > treshold_color){
+        Serial.println("found");
         return;
       }
     }
-    Serial.println("not color");
+    Serial.println("no ball");
 }
